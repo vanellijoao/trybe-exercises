242f0d5b2b96e6c92ff89a6eb2b6b23eb30bed29
Joao Vanelli <vanellijoao@gmail.com>
2021-02-24 18:35:02 -0300

    Bloco 4.1 - Exercicio 1

---
+++ bloco4/dia1/script.js
@@ -0,0 +1,26 @@
+// Exercício 1
+
+let a = 10;
+let b = 5;
+
+// Adição
+let add = a + b;
+console.log(add);
+
+// Subtração
+let sub = a - b;
+console.log(sub);
+
+// Multiplicação
+let mult = a * b;
+console.log(mult);
+
+// Divisão
+let div = a / b;
+console.log(div);
+
+// Módulo
+let mod = a % b;
+console.log(mod);
+
+//-----------------
