402a57ab68b4a90fd3fce6758ce5c4cfca37507a
Joao Vanelli <vanellijoao@gmail.com>
2021-02-24 19:18:53 -0300

    Bloco 4.1 - Exercicio 6

---
+++ bloco4/dia1/ex6.js
@@ -0,0 +1,27 @@
+let piece = "kiNg";
+
+let pieceLower = piece.toLowerCase(piece)
+console.log(pieceLower)
+
+switch (pieceLower) {
+    case "bishop":
+        console.log("Bishops only move diagonally");
+        break;
+    case "pawn":
+        console.log("Pawns can move 2 squares foward the first time, then only 1 step foward. Pawns must 'kill' diagonally");
+        break;
+    case "knight":
+        console.log("Knights move in a L shape");
+        break;
+    case "rook":
+        console.log("Rooks move any amount of square fowards, backwards, left or right (if possible)");
+        break;
+    case "king":
+        console.log("King can move one square to any side");
+        break;
+    case "queen":
+        console.log("Queen can move any amount of squares to any side");
+        break;
+    default:
+        console.log("This is no chess piece!")
+}
