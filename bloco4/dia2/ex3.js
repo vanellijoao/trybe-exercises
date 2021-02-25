9f8cf946d164de0f1588cdeec9ea0817a5fccce7
Joao Vanelli <vanellijoao@gmail.com>
2021-02-25 15:36:37 -0300

    Bloco 4.2 - Exercicio 3

---
+++ bloco4/dia2/ex3.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let avg = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i]
+};
+
+avg = sum / (numbers.length);
+
+console.log(avg)
