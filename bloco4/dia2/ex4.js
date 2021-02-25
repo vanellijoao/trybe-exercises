17c5f8d46b6eeaebee943e8fd686ee134d71d4bf
Joao Vanelli <vanellijoao@gmail.com>
2021-02-25 15:38:27 -0300

    Bloco 4.2 - Exercicio 4

---
+++ bloco4/dia2/ex4.js
@@ -0,0 +1,16 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let avg = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i]
+};
+
+avg = sum / (numbers.length);
+
+
+if (avg > 20) {
+    console.log("valor maior que 20")
+} else {
+    console.log("valor menor que 20")
+}
