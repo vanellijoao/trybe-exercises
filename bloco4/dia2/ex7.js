fa710021db64356ae9a14355d6f7b543487ad1b2
Joao Vanelli <vanellijoao@gmail.com>
2021-02-25 15:57:49 -0300

    Bloco 4.2 - Exercicio 7

---
+++ bloco4/dia2/ex7.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let comparison = 1000;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (comparison > numbers[i]) {
+        comparison = numbers[i];
+    } else {
+        comparison = comparison;
+    }
+}
+
+console.log(comparison);
