069feada0f8358fca7c92152f068a5f727373a2b
Joao Vanelli <vanellijoao@gmail.com>
2021-02-26 16:00:17 -0300

    Bloco 4.3 - Exercicio 1

---
+++ bloco4/dia3/ex1.js
@@ -0,0 +1,12 @@
+let n = 2;
+let asterisc = "*";
+let square = "";
+
+
+for (let i = 1; i <= n; i += 1) {
+    square = square += asterisc;
+}
+
+for (let i = 1; i <= n; i += 1) {
+    console.log(square)
+}
