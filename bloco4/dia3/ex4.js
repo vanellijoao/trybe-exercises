0b4c1254b318ade47ebeedba268f0d91c1a34d81
Joao Vanelli <vanellijoao@gmail.com>
2021-03-01 11:30:32 -0300

    Bloco 4.3 - Exercicio 4

---
+++ bloco4/dia3/ex4.js
@@ -0,0 +1,22 @@
+// n = 5
+
+//   *
+//  ***
+// *****
+
+let n = 9;
+let space = " ";
+let asterisc = "*"
+let string = ""
+
+if (n % 2 > 0) {
+    for (let i = 1; i <= n; i += 2) {
+        string = space.repeat((n-i/2)) + asterisc.repeat(i) +space.repeat((n-i/2))
+        console.log(string)
+    }
+} else {
+    for (let i = 2; i <= n; i += 2) {
+        string = space.repeat((n-i/2)) + asterisc.repeat(i) +space.repeat((n-i/2))
+        console.log(string)
+    }
+}
