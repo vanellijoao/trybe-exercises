6e134854263ddd4ea86a1d409ef9a2ad0f36caf9
Joao Vanelli <vanellijoao@gmail.com>
2021-03-01 20:08:42 -0300

    Bloco 4.4 - Parte 1 Exercicio 5

--- bloco4/dia4/ex4.js
+++ bloco4/dia4/ex4.js
@@ -3,9 +3,20 @@ let info = {
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
   };
+
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+}
   
 info.recorrente = "Sim";
 
 for (let key in info) {
-    console.log(info[key])
+    if (info[key] === info2[key]) {
+        console.log("Ambos recorrentes")
+    } else {
+        console.log(`${info[key]} e ${info2[key]}`)
+    }
 };
