d6847de9d90af8091d180de2f7d223c4a358ab8e
Joao Vanelli <vanellijoao@gmail.com>
2021-03-02 11:07:53 -0300

    Bloco 4.4 - Parte 2 - Ex5 - Numero que mais se repete

---
+++ bloco4/dia4/parte2/ex2-5.js
@@ -0,0 +1,41 @@
+let numbers = [2, 3, 2, 5, 8, 2, 3];
+
+
+function mostRepeatedNumber (array) {
+// Criando uma array representando o quanto cada número se repetiu    
+    let number = 0;
+    let times = [];
+
+    for (i = 0; i < array.length; i += 1) {
+        for (j = 0; j < array.length; j += 1) {
+            if (array[i] == array[j]) {
+                number += 1;
+            }
+        };
+
+        times.push(number);
+
+        number = 0;
+    }
+
+// Descobrindo o maior valor da array nova
+    let comparison = 0;
+        
+    for (let i = 0; i < times.length; i+= 1) {
+        if (comparison < times[i]) {
+            comparison = times[i];
+        } else {
+            comparison = comparison;
+        }
+    };
+
+// Pegando o número que mais se repetiu baseado no index do maior número da array nova
+    let index = 0;
+
+    index = times.indexOf(comparison)
+
+    return numbers[index]
+}
+
+console.log(mostRepeatedNumber(numbers))
+
